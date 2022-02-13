b6f856e1d7a5feb8eeb07bc08bade832768dc8bf
churabou <churabou@users.noreply.github.com>
2022-02-13 11:45:41 +0000

    deploy: 95c05e6efed8e9afbb83f89527d7607b62add975

---
+++ _next/static/SMksfBBwA6R2o-EPmWjl4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fb948bb472f7d244.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
